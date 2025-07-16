f0ca92b4411410768f8ce63ce7da9aa8a58ccf3b
hellodataTataouine <contact@hellodata.tn>
2025-07-16 15:40:27 +0100

    up

--- app/Http/Controllers/Frontend/AskBookController.php
+++ app/Http/Controllers/Frontend/AskBookController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Modules\Frontend\app\Models\ContactSection;
 use App\Traits\MailSenderTrait;
+use Illuminate\Support\Facades\Cache;
+use Modules\GlobalSetting\app\Models\Setting;
 
 class AskBookController extends Controller
 {
@@ -28,14 +30,24 @@ class AskBookController extends Controller
 
         $bookRequest =BookRequest::create($validated);
         MailSenderTrait::setMailConfig();
+        if (Cache::has('setting')) {
+            $email_setting = Cache::get('setting');
+        } else {
+            $setting_info = Setting::get();
+            $setting = [];
+            foreach ($setting_info as $setting_item) {
+                $setting[$setting_item->key] = $setting_item->value;
+            }
+            $email_setting = (object) $setting;
+        }
         Mail::raw("
             Nouvelle demande de livre reçue :
             Nom : {$bookRequest->name}
             Email : {$bookRequest->email}
             Téléphone : {$bookRequest->phone}
             Message : {$bookRequest->message}
-        ", function ($message) {
-            $message->to(env('MAIL_FROM_ADDRESS'))
+        ", function ($message) use($email_setting) {
+            $message->to($email_setting->mail_username)
                     ->subject('Nouvelle demande de livre');
         });
         return response()->json([
